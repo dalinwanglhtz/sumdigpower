7ec7715f7ff8b2f5dce667ac42606299c58e6774
Dale Wang <dalinwanglhtz@gmail.com>
2019-12-12 23:21:25 +1100

    fixed bug with Math.power, indexOf method could return value of wrong position, need to increment index as power

--- src/com/codewardev/SumDigPower.java
+++ src/com/codewardev/SumDigPower.java
@@ -1,22 +1,28 @@
 package com.codewardev;
 
-// https://www.codewars.com/kata/take-a-number-and-sum-its-digits-raised-to-the-consecutive-powers-and-dot-dot-dot-eureka/train/java
+//https://www.codewars.com/kata/take-a-number-and-sum-its-digits-raised-to-the-consecutive-powers-and-dot-dot-dot-eureka/train/java
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SumDigPower {
 
 	public static List<Long> sumDigPow(long a, long b) {
-		
-		for(long i=a; i<b; i++) {
+		List<Long> result = new ArrayList<Long>();
+		for(long i=a; i<=b; i++) {
 			String iString = Long.toString(i);
-			int sum = iString.chars()
-					.map(x->Character.getNumericValue(x))
-					.map(x->(int)Math.pow(Double.valueOf(x), Double.valueOf(iString.indexOf(x+"")+1))).sum();
-			System.out.println("Sum of "+i+"="+sum);
+			int sum = 0;
+			for(int j=0; j<iString.length(); j++) {
+				String[] iArray = iString.split("");
+				sum += Math.pow(Double.valueOf(iArray[j]), Double.valueOf(j)+1);
+			}
+			if(i==sum) {
+				System.out.println("Found for "+i+"="+sum);
+				result.add(i);
+			}
 		}
 		
-		return null;
+		return result;
 	}
 
 }
