f131b33bc97e50d2823dde92df1ab6a48448fdbb
Dale Wang <dalinwanglhtz@gmail.com>
2019-12-13 20:59:25 +1100

    refactor for loop

--- src/com/codewardev/SumDigPower.java
+++ src/com/codewardev/SumDigPower.java
@@ -1,21 +1,22 @@
 package com.codewardev;
 
-//https://www.codewars.com/kata/take-a-number-and-sum-its-digits-raised-to-the-consecutive-powers-and-dot-dot-dot-eureka/train/java
+// https://www.codewars.com/kata/take-a-number-and-sum-its-digits-raised-to-the-consecutive-powers-and-dot-dot-dot-eureka/train/java
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 public class SumDigPower {
 
 	public static List<Long> sumDigPow(long a, long b) {
 		List<Long> result = new ArrayList<Long>();
-		for(long i=a; i<=b; i++) {
+		
+		for(long i=a; i<b; i++) {
 			String iString = Long.toString(i);
-			int sum = 0;
-			for(int j=0; j<iString.length(); j++) {
-				String[] iArray = iString.split("");
-				sum += Math.pow(Double.valueOf(iArray[j]), Double.valueOf(j)+1);
-			}
+			String[] iArray = iString.split("");
+			int sum = IntStream.range(0, iString.length())
+			.map(x->(int)Math.pow(Double.valueOf(iArray[x]), Double.valueOf(x)+1)).sum();
 			if(i==sum) {
 				System.out.println("Found for "+i+"="+sum);
 				result.add(i);
